1e088d01b7eb4eb686d590e4c09c067cc674c8b0
anand5au <anandkumar.dhandapani@gmail.com>
2016-03-15 01:11:21 -0700

    Create DeadLockGuardedBlock.java

---
+++ Multithreading/src/thread/DeadLockGuardedBlock.java
@@ -0,0 +1,55 @@
+class DeadLockGuardedBlock
+{
+	static class Friend
+	{
+		private final String name;
+		public boolean bowing;
+
+		public Friend(String name)
+		{
+			this.name = name;
+			bowing = false;
+		}
+
+		public String getName()
+		{
+			return this.name;
+		}
+
+		public void bow(Friend bower)
+		{
+		    while(bower.bowing);
+			
+		    bowing = true;
+			System.out.format("%s: %s" + "  has bowed to me!%n", this.name, bower.getName());
+			bower.bowBack(this);
+			bowing = false;
+		}
+
+		public void bowBack(Friend bower)
+		{
+			System.out.format("%s: %s" + " has bowed back to me!%n", this.name, bower.getName());
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		final Friend alphonse = new Friend("Alphonse");
+		final Friend gaston = new Friend("Gaston");
+		new Thread(new Runnable()
+		{
+			public void run()
+			{
+				alphonse.bow(gaston);
+			}
+		}).start();
+		
+		new Thread(new Runnable()
+		{
+			public void run()
+			{
+				gaston.bow(alphonse);
+			}
+		}).start();
+	}
+}
